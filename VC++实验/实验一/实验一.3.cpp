7da72258abc181b41e8d2c1027dcd1b4565b0359
code-killer <txq0917@126.com>
2018-09-14 23:54:00 +0800

    实验一

--- VC++实验/实验一/实验一.3.cpp
+++ VC++实验/实验一/实验一.3.cpp
@@ -1,36 +1,36 @@
 #include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
 using namespace std;
 class CBook{
-private: 
+private:
 		int quantity;
 		int price;
 public:
-	Student(char number[],char name[],float grade);
+	CBook(int data);
 	void Show();
 };
-Student::Student(char number[],char name[],float grade)
+CBook::CBook(int data)
 {
-	int i = 0;
-	for(i = 0;number[i]!='\0';i++)studentNumber[i] = number[i];
-	studentNumber[i] = '\0';
-	for(i = 0;number[i]!='\0';i++)studentName[i] = name[i];
-	studentGrade = grade;
-	allStudentNumber++;
-	allGrade += grade;
+	quantity = data;
+	price = quantity*10;
 }
 
-void Student::Show()
+void CBook::Show()
 {
-	cout << studentNumber << " " << studentName << " " << studentGrade<<endl;
-} 
+	cout << "quantity*price的值是:" << quantity*price<<endl;
+}
 
 int main()
 {
-	Student a[5]={Student("P31223331","你好",78),Student("P31223331","你好",78),Student("P31223331","你好",78),Student("P31223331","你好",78),Student("P31223331","你好",78)};
+	CBook a[5]={CBook(25),CBook(34),CBook(12),CBook(37),CBook(88)};
 	for(int i = 0 ;i<5;i++)
 		a[i].Show();
-	cout << "总共有:" << allStudentNumber << "个学生" << endl;
-	cout << "总成绩为" << allGrade << endl;
+    CBook *p;
+    p = a;
+    for(int i = 4;i >= 0;i--)
+        (*(p+i)).Show();
+
 	getchar();
 	return 0;
 }
