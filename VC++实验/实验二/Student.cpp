507a6ed5d14edd31381eceb7e4f4f215470bc45c
code-killer <txq0917@126.com>
2018-10-12 20:04:49 +0800

    实验二

---
+++ VC++实验/实验二/Student.cpp
@@ -0,0 +1,18 @@
+﻿#include"student.h"
+#include<fstream>
+int main() {
+	CStudent stu1("小a",23444123,79);
+	CStudent stu2("小B",432423532423,99);
+    CStudent stu3("小C",324234234,87.8);
+    CStudent stu4("小D",324234234,87.8);
+    CStudent stu5("小E",324234234,87.8);
+	fstream file1;
+	file1.open("student.dat",ios::in|ios::out|ios::binary);
+	file1 << stu1<<stu2<<stu3<<stu4<<stu5;
+	CStudent* one = new CStudent("",0,0);
+	const int size = 13;
+	file1.seekp(0);
+	file1>>*one;
+	one->print();//seekp：设置输出文件流的文件流指针位置seekg：设置输入文件流的文件流指针位置
+	return 0;
+}
---
+++ VC++实验/实验二/Student.h
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+class CStudent{
+private:
+	char sName[5];
+	int sNumber;
+	float sGrade;
+public:
+	CStudent() {}
+	CStudent(const char name[],int number,float grade){
+		strcpy((*this).sName, name);
+		(*this).sNumber = number;
+		(*this).sGrade = grade;
+	}
+	void print() {
+		cout << "姓名:" << sName << "学号:" << sNumber << "成绩:" << sGrade << endl;
+	}
+	friend ostream& operator<<(ostream& os, CStudent& stu) {
+		os.write(stu.sName, 5);
+		os.write((char*)&stu.sNumber, 4);
+		os.write((char*)&stu.sGrade, 4);
+		return os;
+	}
+	friend istream& operator>>(istream& is, CStudent& stu) {
+		char name[5];
+		is.read(name, 5);
+		is.read((char*)&stu.sNumber, 4);
+		is.read((char*)&stu.sGrade, 4);
+		strcpy(stu.sName, name);
+		return is;
+	}
+};
