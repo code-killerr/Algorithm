4bd2b3a36f1e8af52899578fa77fe3871dc4bd36
code-killer <txq0917@126.com>
2018-11-28 23:29:27 +0800

    java

--- 面向对象/static.java
+++ 面向对象/static.java
@@ -1,10 +1,17 @@
 //static一般用于公共数据的存储节省内存空间,一般用于被对象共享,除了被对象调用还可以被类名调用
-//用p.XX调用
+//用p.XX调用,静态函数只能调用静态成员，非静态函数可以调用静态成员和非静态成员，属于类变量
+//静态成员生命周期过长，同时具有局限性
+//类被卸载时，静态变量被销毁，并释放内存空间。static变量的生命周期取决于类的生命周期
+//静态变量存储于方法区/共享区/数据区
 class A{
 	String name;
 	static String sta = "aaa";
 	void print(){
 		System.out.println(name + "->" + sta);
+	}
+	static void prints(){
+		//System.out.println(name + "->" + sta);会报错无法调用非静态成员name
+		System.out.println("sta="+sta);
 	}
 	
 }
